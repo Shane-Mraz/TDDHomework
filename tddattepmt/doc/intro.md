46ae1e55b1fb40efeae040d05e06edefcccd3b95
Shane Mraz <Shane.Mraz@Ticketmaster.com>
2015-11-03 17:23:12 -0800

    created lein project

---
+++ tddattepmt/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tddattepmt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ tddattepmt/test/tddattepmt/core_test.clj
@@ -0,0 +1,7 @@
+(ns tddattepmt.core-test
+  (:require [clojure.test :refer :all]
+            [tddattepmt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
