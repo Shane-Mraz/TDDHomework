03c443d0ea7dda8f45f6bfd02bdba47af5f921ac
Shane Mraz <Shane.Mraz@Ticketmaster.com>
2015-11-09 10:24:50 -0800

    trying again with character index

--- diamond02/try02/src/try02/core.clj
+++ diamond02/try02/src/try02/core.clj
@@ -1,6 +1,10 @@
 (ns try02.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require '[clojure.string :as string])
+
+(defn char-index [letter] (- (int (first (char-array letter))) 65))
+
+(defn inner-part [letter]
+  (let [index (char-index letter)]
+    (cond (= 0 index) letter
+          :else (str letter (string/join "" (repeat (- (* 2 index) 1) "-")) letter))))
--- diamond02/try02/test/try02/core_test.clj
+++ diamond02/try02/test/try02/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [try02.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest char-indexing
+  (testing "A"
+    (is (= 0 (char-index "A"))))
+  (testing "B"
+    (is (= 1 (char-index "B"))))
+  (testing "C"
+    (is (= 2 (char-index "C"))))
+  (testing "Z"
+    (is (= 25 (char-index "Z")))))
