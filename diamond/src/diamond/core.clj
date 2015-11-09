b585b2abe95f542500e41099d0c000327b63a741
Shane Mraz <Shane.Mraz@Ticketmaster.com>
2015-11-09 10:07:05 -0800

    trying to big of a bite, starting over again

--- diamond/src/diamond/core.clj
+++ diamond/src/diamond/core.clj
@@ -6,11 +6,74 @@
 
 (defn position-of [letter] (inc (- (int letter) (int \a))))
 
+(defn dashes [n] (repeat n \-))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;(defn char-index [letter] (- (int (first (char-array letter))) 65))
+(defn drop-first-column [sequence-of-sequences-of-chars]
+  (map rest sequence-of-sequences-of-chars))
 
-;(defn inner-part [letter] 
-;	(let [index (char-index letter)]
-;		(cond (= 0 index) letter
-;			:else (str letter (string/join "" (repeat (- (* 2 index) 1) "-")) letter))))
+(defn drop-first-row [sequence-of-sequences-of-chars]
+  (drop 1 sequence-of-sequences-of-chars))
+
+(defn reverse-every-row [sequence-of-sequences-of-chars]
+  (map reverse sequence-of-sequences-of-chars))
+
+(defn core-letter-pattern-for [letter]
+  (concat
+    (dashes (dec (position-of letter)))
+    (list letter)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn core-right-quadrant-pattern-for [letter]
+  (map core-letter-pattern-for (take (position-of letter) alphabet)))
+
+(defn trailing-dashes-appender-for [max-dash-count]
+  (fn [index line]
+    (concat
+      line
+      (dashes (dec (- max-dash-count index))))))
+
+(defn flip-bottom-up [top-half-of-diamond]
+  (reverse top-half-of-diamond))
+
+(defn join-together-side-by-side [top-left-quadrant top-right-quadrant]
+  (map concat top-left-quadrant top-right-quadrant))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn create-top-right-quadrant-for [letter]
+  (let [number-of-letters (position-of letter)]
+    (map-indexed (trailing-dashes-appender-for number-of-letters) (core-right-quadrant-pattern-for letter))))
+
+(defn drop-first-column-and-reverse-every-row-of [top-right-quadrant]
+  (->> top-right-quadrant
+       drop-first-column
+       reverse-every-row))
+
+(defn flip-bottom-up-and-drop-first-row-of [top-half-of-diamond]
+  (drop-first-row (flip-bottom-up top-half-of-diamond)))
+
+(defn put-one-on-top-of-the-other [top-half-of-diamond bottom-half-of-diamond]
+  (concat top-half-of-diamond bottom-half-of-diamond))
+
+(defn strings-for [sequences-of-chars]
+  (map #(apply str %) sequences-of-chars))
+
+(defn display [sequence-of-sequences-of-chars]
+  (doseq [line (strings-for sequence-of-sequences-of-chars)]
+    (println line)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn print-diamond [letter]
+  (let [top-right-quadrant (create-top-right-quadrant-for letter)
+        top-left-quadrant (drop-first-column-and-reverse-every-row-of top-right-quadrant)
+        top-half-of-diamond (join-together-side-by-side top-left-quadrant top-right-quadrant)
+        bottom-half-of-diamond (flip-bottom-up-and-drop-first-row-of top-half-of-diamond)
+        diamond (put-one-on-top-of-the-other top-half-of-diamond bottom-half-of-diamond)]
+    (display diamond)))
+
+(defn -main []
+  (print-diamond \E))
--- diamond/test/diamond/core_test.clj
+++ diamond/test/diamond/core_test.clj
@@ -9,32 +9,157 @@
 (deftest position-of-z
   (is (= 26 (position-of \z))))
 
-;(deftest no-dashes
-; (is (= () (dashes 0))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;(defttest one-dash
-;  (is (= (seq "-") (dashes 1))))
+(deftest no-dashes
+  (is (= () (dashes 0))))
 
-;(deftest five-dahses
-;  (is (= (seq "-----") (dashes 5))))
+(deftest one-dash
+  (is (= (seq "-") (dashes 1))))
 
+(deftest five-dashes
+  (is (= (seq "-----") (dashes 5))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(deftest core-letter-pattern-for-A
+  (is (= (seq "A") (core-letter-pattern-for \A))))
 
-; Defining character index
-;(deftest char-indexing
-;	(testing "a"
-;		(is (= 1 (char-index "a"))))
-;	(testing "b"
-;		(is (= 2 (char-index "b"))))
-;	(testing "z"
-;		(is (= 26 (char-index "z"))))
-;	)
+(deftest core-letter-pattern-for-B
+  (is (= (seq "-B") (core-letter-pattern-for \B))))
 
-;Test for the inner portion of the diamond
-;(deftest inner-part-building
-;	(testing "a"
-;		(is (= "a" (inner-part "a"))))
-;	(testing "b"
-;		(is (= "b-b" (inner-part "b"))))
-;	)
+(deftest core-letter-pattern-for-F
+  (is (= (seq "-----F") (core-letter-pattern-for \F))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest core-right-quadrant-pattern-for-A
+  (is (= (list (seq "A")) (core-right-quadrant-pattern-for \A))))
+
+(deftest core-right-quadrant-pattern-for-B
+  (is (= (list (seq "A")
+               (seq "-B")) (core-right-quadrant-pattern-for \B))))
+
+(deftest core-right-quadrant-pattern-for-F
+  (is (=
+         (list (seq "A")
+               (seq "-B")
+               (seq "--C")
+               (seq "---D")
+               (seq "----E")
+               (seq "-----F"))
+        (core-right-quadrant-pattern-for \F))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest appending-trailing-dashes
+    (is (= (list (seq "A-----")
+                 (seq "-B----")
+                 (seq "--C---")
+                 (seq "---D--")
+                 (seq "----E-")
+                 (seq "-----F"))
+            (map-indexed
+              (trailing-dashes-appender-for (position-of \F))
+              (list (seq "A")
+                    (seq "-B")
+                    (seq "--C")
+                    (seq "---D")
+                    (seq "----E")
+                    (seq "-----F"))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest creating-top-right-quadrant
+  (is (= (list (seq "A-----")
+               (seq "-B----")
+               (seq "--C---")
+               (seq "---D--")
+               (seq "----E-")
+               (seq "-----F"))
+          (create-top-right-quadrant-for \F))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest creating-top-left-quadrant-from-top-right-quadrant
+  (is (= (list (seq "-----")
+               (seq "----B")
+               (seq "---C-")
+               (seq "--D--")
+               (seq "-E---")
+               (seq "F----"))
+          (drop-first-column-and-reverse-every-row-of
+            (list (seq "A-----")
+                  (seq "-B----")
+                  (seq "--C---")
+                  (seq "---D--")
+                  (seq "----E-")
+                  (seq "-----F"))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest creating-top-half-of-diamond-with-top-left-quadrant-and-top-right-quadrant
+  (is (= (list (seq "-----A-----")
+               (seq "----B-B----")
+               (seq "---C---C---")
+               (seq "--D-----D--")
+               (seq "-E-------E-")
+               (seq "F---------F"))
+          (join-together-side-by-side
+            (list (seq "-----")
+                  (seq "----B")
+                  (seq "---C-")
+                  (seq "--D--")
+                  (seq "-E---")
+                  (seq "F----"))
+            (list (seq "A-----")
+                  (seq "-B----")
+                  (seq "--C---")
+                  (seq "---D--")
+                  (seq "----E-")
+                  (seq "-----F"))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest creating-bottom-half-of-diamond-from-top-half-of-diamond
+  (is (= (list
+           (seq "-E-------E-")
+           (seq "--D-----D--")
+           (seq "---C---C---")
+           (seq "----B-B----")
+           (seq "-----A-----"))
+         (flip-bottom-up-and-drop-first-row-of
+           (list
+             (seq "-----A-----")
+             (seq "----B-B----")
+             (seq "---C---C---")
+             (seq "--D-----D--")
+             (seq "-E-------E-")
+             (seq "F---------F"))))))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest creating-diamond-with-top-half-and-bottom-half
+  (is (= (list (seq "-----A-----")
+               (seq "----B-B----")
+               (seq "---C---C---")
+               (seq "--D-----D--")
+               (seq "-E-------E-")
+               (seq "F---------F")
+               (seq "-E-------E-")
+               (seq "--D-----D--")
+               (seq "---C---C---")
+               (seq "----B-B----")
+               (seq "-----A-----"))
+         (put-one-on-top-of-the-other
+           (list (seq "-----A-----")
+                 (seq "----B-B----")
+                 (seq "---C---C---")
+                 (seq "--D-----D--")
+                 (seq "-E-------E-")
+                 (seq "F---------F"))
+           (list (seq "-E-------E-")
+                 (seq "--D-----D--")
+                 (seq "---C---C---")
+                 (seq "----B-B----")
+                 (seq "-----A-----"))))))
